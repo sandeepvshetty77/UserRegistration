2d9901842ebae28417672246d4946f6f385309aa
Sandeep Shetty <sandeepvs@yahoo.com>
2020-07-23 11:22:31 +0100

    JWT generation code with displaying in UI via ViewBag for checking

--- UserRegistration/Controllers/HomeController.cs
+++ UserRegistration/Controllers/HomeController.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using UserRegistration.Models;
 
 namespace UserRegistration.Controllers
@@ -12,15 +17,51 @@ namespace UserRegistration.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private IConfiguration _config;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
         {
+            _config = config;           
             _logger = logger;
         }
 
+        [HttpGet]
         public IActionResult Index()
-        {
+        {           
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(UserViewModel userViewModel)
+        {
+            string token = String.Empty;
+            if (ModelState.IsValid)
+            {
+                token = GenerateJSONWebToken(userViewModel);
+                ViewBag.Data = token;
+            }
+
+            return View();
+        }
+
+        private string GenerateJSONWebToken(UserViewModel userViewModel)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var securityToken = new JwtSecurityToken(_config["Jwt:Issuer"],
+              _config["Jwt:Issuer"],
+              new Claim[] {
+                    new Claim(ClaimTypes.NameIdentifier, userViewModel.Id.ToString())
+                 },
+              null,
+              expires: DateTime.Now.AddMinutes(120),
+              signingCredentials: credentials);
+
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.WriteToken(securityToken);
+
+            return token;
         }
 
         public IActionResult Privacy()
